915193092776c84eba43f06b7195ae134f35a9d0
Brian Mann <bmann505@gamil.com>
2017-10-17 08:58:48 -0600

    cors

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ const cors = require('cors');
 const port = process.env.PORT || 8080;
 const routes = require('./db/routes');
 const bodyParser = require('body-parser');
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Headers', 'Authorization');
+  next();
+})
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use('/', routes);
+
+
+app.listen(port);
