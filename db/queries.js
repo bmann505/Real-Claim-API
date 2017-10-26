4d4dbd6098330b788b4766368eb50995fe4cf37a
Brian Mann <bmann505@gamil.com>
2017-10-25 22:45:01 -0600

    contractor data

--- db/queries.js
+++ db/queries.js
@@ -21,8 +21,8 @@ module.exports = {
   },
   claimByOwner: (id) => {
     return knex('claim')
-      .select('claim.id', 'claim.estimate', 'claim.status', 'claim.value', 'claim.address', 'claim.user_id',
-        'claim.contractor_id', 'claim.adjustor_id')
+      .select('claim.id', 'claim.claim-description', 'claim.estimate', 'claim.status', 'claim.value', 'claim.address', 'claim.user_id',
+        'claim.contractor_id', 'claim.adjustor_id', )
       .innerJoin('user', 'user.id', 'claim.user_id')
       .where('user.id', id)
       .then(claims => {
@@ -46,6 +46,33 @@ module.exports = {
           })
         })
       })
-
+  },
+  claimByContractor: (id) => {
+    return knex('claim')
+      .select('claim.id', 'claim.claim-description', 'claim.estimate', 'claim.status', 'claim.value', 'claim.address', 'claim.user_id',
+        'claim.contractor_id', 'claim.adjustor_id', )
+      .innerJoin('user', 'user.id', 'claim.contractor_id')
+      .where('user.id', id)
+      .then(claims => {
+        let userRequests = []
+        claims.forEach(claim => {
+          userRequests.push(knex('user')
+            .select('user.name')
+            .where('user.id', claim.user_id).first())
+          userRequests.push(knex('user')
+            .select('user.name')
+            .where('user.id', claim.adjustor_id).first())
+        })
+        return Promise.all(userRequests).then(users => {
+          return claims.map((claim, i) => {
+            const index = i * 2
+            claim.user = users[index].name
+            delete claim.user_id
+            claim.adjustor = users[index + 1].name
+            delete claim.adjustor_id
+            return claim
+          })
+        })
+      })
   }
 }
