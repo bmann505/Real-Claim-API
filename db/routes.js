a890dfab8a866a57b7dc765765617d112ba090b0
Brian Mann <bmann505@gamil.com>
2017-10-27 11:09:45 -0600

    S3

--- db/routes.js
+++ db/routes.js
@@ -4,7 +4,15 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const queries = require('./queries');
 const knex = require('./connection')
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v4');
+const upload = require('multer')();
 require('dotenv').config();
+AWS.config.update({
+  accessKeyId: process.env.S3_KEY,
+  secretAccessKey: process.env.S3_SECRET
+});
+const s3 = new AWS.S3();
 
 function validUser(user) {
   let validName = typeof user.name === 'string' && user.name.trim() !== '' && user.name !== null;
@@ -126,6 +134,21 @@ router.post('/signup', (req, res, next) => {
     })
   }
 });
+
+router.post('/image', upload.single('image'), (req, res) => {
+  let id = uuid();
+  s3.putObject({
+    Bucket: process.env.S3_BUCKET,
+    Key: id,
+    Body: new Buffer(req.file.buffer)
+  }, err => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.json(id)
+    }
+  });
+})
 
 
 
